6ff89cf7ef6ff076625ceb78ebedd0a6f0d1af1a
oruuke <goober@oruuke.com>
2025-10-25 22:57:39 +1000

    embedded all listing files so binary actually fr fr works as standalone

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use book_tldr::File;
+use book_tldr::Listing;
 use book_tldr::get_files;
 use clap::Parser;
+use fuzzy_matcher::FuzzyMatcher;
+use fuzzy_matcher::skim::SkimMatcherV2;
+use include_dir::{Dir, include_dir};
 use std::error::Error;
-use std::fs;
 use std::process;
 
 // get args from user, defaulting to universally matching string
@@ -19,12 +21,14 @@ struct Args {
     status: String,
 }
 
+static ASSETS: Dir = include_dir!("$CARGO_MANIFEST_DIR/listings");
+
 // start program and error if needed
 fn main() {
     let args = Args::parse();
 
     if let Err(e) = run(args) {
-        eprintln!("oopsie: {e}");
+        eprintln!("oopsie! error running: {e}");
         process::exit(1);
     }
 }
@@ -35,13 +39,13 @@ fn run(args: Args) -> Result<(), Box<dyn Error>> {
     let files = get_files();
 
     // iterate over files and filter results across all fields
-    let results: Vec<&'static File> = files
+    let results: Vec<&'static Listing> = files
         .iter()
         .filter(|a| {
-            a.chapter.contains(&args.chapter)
-                && a.description.contains(&args.description)
-                && a.listing.contains(&args.listing)
-                && a.status.contains(&args.status)
+            check_match(a.chapter, &args.chapter)
+                && check_match(a.description, &args.description)
+                && check_match(a.listing, &args.listing)
+                && check_match(a.status, &args.status)
         })
         .map(|a| a)
         .collect();
@@ -54,13 +58,14 @@ fn run(args: Args) -> Result<(), Box<dyn Error>> {
         }
     // print all listing code if just one result
     } else if results.len() == 1 {
-        let contents =
-            fs::read_to_string("listings/".to_owned() + &results[0].listing.to_string() + ".md");
         println!("full listing:\n");
         print_info(results[0]);
 
-        if let Ok(line) = contents {
-            println!("{line}");
+        let listing_file = results[0].listing.to_owned() + ".md";
+        if let Some(s) = read_text(&listing_file) {
+            println!("{s}")
+        } else {
+            eprintln!("oopsie! could not find file");
         }
     } else {
         println!("none found!");
@@ -69,7 +74,25 @@ fn run(args: Args) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn print_info(file: &File) {
+fn read_text(name: &str) -> Option<&'static str> {
+    ASSETS.get_file(name)?.contents_utf8()
+}
+
+fn check_match(source: &str, query: &str) -> bool {
+    if query == "" || source.contains(query) {
+        return true;
+    }
+    let matcher = SkimMatcherV2::default();
+    let test = matcher.fuzzy_indices(source, query);
+    if test != None {
+        let (score, _indices) = test.unwrap();
+        return score >= 50;
+    } else {
+        return false;
+    }
+}
+
+fn print_info(file: &Listing) {
     println!("chapter: {}", file.chapter);
     println!("description: {}", file.description);
     println!("listing: {}, status: {}\n", file.listing, file.status);
